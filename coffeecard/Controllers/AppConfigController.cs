4506f04e339cced34ead9f3766a61aa0a91258cb
Kasper <klar@itu.dk>
2019-10-12 16:12:18 +0200

    Added AppConfigController, DTO and Service

---
+++ coffeecard/Controllers/AppConfigController.cs
@@ -0,0 +1,33 @@
+﻿using coffeecard.Models.DataTransferObjects.AppConfig;
+using coffeecard.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace coffeecard.Controllers
+{
+    [ApiVersion("1")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [Authorize]
+    [ApiController]
+    public class AppConfigController : ControllerBase
+    {
+        private IAppConfigService _appConfigService;
+
+        public AppConfigController(IAppConfigService appConfigService)
+        {
+            _appConfigService = appConfigService;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<AppConfigDTO>> Get()
+        {
+            var appConfig = _appConfigService.RetreiveConfiguration();
+            if(appConfig == null) return NotFound();
+            return Ok(appConfig);
+        }
+    }
+}
---
+++ coffeecard/Models/DataTransferObjects/AppConfig/AppConfigDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace coffeecard.Models.DataTransferObjects.AppConfig
+{
+    public class AppConfigDTO
+    {
+        public string EnvironmentType { get; set; }
+        public string MerchantId { get; set; }
+    }
+}
