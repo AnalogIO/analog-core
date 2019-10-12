435b43d5a00a3946c890821671133ec4796e7cf0
Kasper <klar@itu.dk>
2019-10-12 16:12:18 +0200

    Added AppConfigController, DTO and Service

---
+++ coffeecard/Services/AppConfigService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using coffeecard.Models.DataTransferObjects.AppConfig;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+
+namespace coffeecard.Services
+{
+    public class AppConfigService : IAppConfigService
+    {
+        private readonly IConfiguration _configuration;
+
+        public AppConfigService(IConfiguration configuration)
+        {
+            _configuration = configuration;          
+        }
+        //TODO throw exception if EnvironmentType and MerchantID is null or empty-string.
+        public AppConfigDTO RetreiveConfiguration()
+        {
+            return new AppConfigDTO
+            {
+                EnvironmentType = _configuration["EnvironmentType"],
+                MerchantId = _configuration["MobilePayMerchantId"]
+            };
+        }
+    }
+}
---
+++ coffeecard/Services/IAppConfigService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using coffeecard.Models.DataTransferObjects.AppConfig;
+
+namespace coffeecard.Services
+{
+    public interface IAppConfigService
+    {
+        AppConfigDTO RetreiveConfiguration();
+    }
+}
