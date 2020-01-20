5b53c6dd16a8ed602c277b968b21d362c639d2a8
Jonas Anker Rasmussen <jonasanker@gmail.com>
2020-01-20 23:12:26 +0100

    Setup CoffeeCardTests project and move CoffeeCard project to its own folder

---
+++ coffeecard/CoffeeCard/Startup.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using CoffeeCard.Helpers;
+using CoffeeCard.Helpers.MobilePay;
+using CoffeeCard.Models;
+using CoffeeCard.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
+
+namespace CoffeeCard
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration, IHostingEnvironment env)
+        {
+            Configuration = configuration;
+            Environment = env;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public IHostingEnvironment Environment { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // TODO: find out the proper way to differ between enviroment based config files
+            services.AddDbContext<CoffeecardContext>(opt =>
+                opt.UseSqlServer(Configuration.GetConnectionString("CoffeecardDatabase")));
+
+            services.AddSingleton<IConfiguration>(provider => Configuration);
+            services.AddSingleton<IHostingEnvironment>(Environment);
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddScoped<IHashService, HashService>();
+            services.AddTransient<ITokenService, TokenService>();
+            services.AddScoped<IAccountService, AccountService>();
+            services.AddScoped<IPurchaseService, PurchaseService>();
+            services.AddScoped<IMapperService, MapperService>();
+            services.AddScoped<IEmailService, EmailService>();
+            services.AddScoped<IProgrammeService, ProgrammeService>();
+            services.AddScoped<IProductService, ProductService>();
+            services.AddScoped<ITicketService, TicketService>();
+            services.AddScoped<IMobilePayService, MobilePayService>();
+            services.AddScoped<ILeaderboardService, LeaderboardService>();
+            services.AddScoped<IAppConfigService, AppConfigService>();
+            services.AddHttpClient<IMobilePayApiHttpClient, MobilePayApiHttpClient>();
+
+            services.AddMvc(options =>
+            {
+                options.Filters.Add(new ApiExceptionFilter());
+            }).SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddApiVersioning();
+
+            services.AddSwaggerDocument(config =>
+            {
+                config.PostProcess = document =>
+                {
+                    document.Info.Version = "v1";
+                    document.Info.Title = "Café Analog CoffeeCard API";
+                    document.Info.Description = "ASP.NET Core web API for the coffee bar Café Analog";
+                    document.Info.TermsOfService = "None";
+                    document.Info.Contact = new NSwag.OpenApiContact
+                    {
+                        Name = "AnalogIO",
+                        Email = "admin@analogio.dk",
+                        Url = "https://github.com/analogio"
+                    };
+                    document.Info.License = new NSwag.OpenApiLicense
+                    {
+                        Name = "Use under MIT",
+                        Url = "https://github.com/AnalogIO/analog-core/blob/master/LICENSE"
+                    };
+                };
+            });
+
+            services.AddMvc().AddJsonOptions(options =>
+            {
+                options.SerializerSettings.DateTimeZoneHandling = DateTimeZoneHandling.Utc;
+            });
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = "bearer";
+                options.DefaultChallengeScheme = "bearer";
+            }).AddJwtBearer("bearer", options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateAudience = false,
+                    ValidateIssuer = false,
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["TokenKey"])),
+                    ValidateLifetime = true,
+                    ClockSkew = TimeSpan.Zero //the default for this setting is 5 minutes
+                };
+                options.Events = new JwtBearerEvents
+                {
+                    OnAuthenticationFailed = context =>
+                    {
+                        if (context.Exception.GetType() == typeof(SecurityTokenExpiredException))
+                        {
+                            context.Response.Headers.Add("Token-Expired", "true");
+                        }
+
+                        return Task.CompletedTask;
+                    }
+                };
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseOpenApi();
+            app.UseSwaggerUi3();
+
+            app.UseAuthentication();
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
