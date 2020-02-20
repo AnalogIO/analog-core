8bb8d53468242edee183222ef2126f15a941520d
Jonas Anker Rasmussen <jonasanker@gmail.com>
2020-02-20 18:18:46 +0100

    Simple PingController for heartbeat

---
+++ coffeecard/CoffeeCard/Controllers/PingController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoffeeCard.Controllers
+{
+    [ApiVersion("1")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiController]
+    public class PingController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("pong");
+        }
+    }
+}
