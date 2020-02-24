66b0b8590309e566431828f84e5132695985e2d3
Jonas Anker Rasmussen <jonasanker@gmail.com>
2020-02-24 15:06:13 +0100

    ProductService unit tests

--- coffeecard/CoffeeCard/Models/Product.cs
+++ coffeecard/CoffeeCard/Models/Product.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace CoffeeCard.Models
@@ -21,5 +23,18 @@ namespace CoffeeCard.Models
         public bool Visible { get; set; }
         [Required]
         public bool BaristasOnly { get; set; }
+
+        protected bool Equals(Product other)
+        {
+            return Id == other.Id && Price == other.Price && NumberOfTickets == other.NumberOfTickets && Name == other.Name && Description == other.Description && ExperienceWorth == other.ExperienceWorth && Visible == other.Visible && BaristasOnly == other.BaristasOnly;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Product)obj);
+        }
     }
 }
