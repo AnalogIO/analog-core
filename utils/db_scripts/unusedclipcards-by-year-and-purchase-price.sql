2a16f74d1d1865d22389a86b42555c15789980fa
Jonas Anker Rasmussen <jonasanker@gmail.com>
2021-02-03 16:04:49 +0100

    SQL sript for Unused clip cards

---
+++ utils/db_scripts/unusedclipcards-by-year-and-purchase-price.sql
@@ -0,0 +1,28 @@
+-- Unused clips cards and their net worth
+-- 
+-- - Group by purchase year
+-- - The value is calculated based on the product at the time of purchase
+
+SELECT
+	YEAR(t.DateCreated) AS 'Purchase Year',
+	p.Id  as 'ProductId',
+	p.Name AS 'Product',
+	Count(t.Id) AS 'Unused tickets',
+	(ph.Price / ph.NumberOfTickets) AS 'Per ticket incl moms', -- Product price as the time of purchase
+	(ph.Price / ph.NumberOfTickets) * COUNT(t.Id) AS 'Subtotal incl moms' -- Subtotal of unused tickets with purchase price
+FROM
+	Tickets t
+INNER JOIN Purchases ph ON
+	ph.Id = t.Purchase_Id
+INNER JOIN Products p ON
+	t.ProductId = p.Id
+WHERE
+	ph.Completed = 1 -- Only look at completed Purchases
+	AND t.IsUsed = 0 -- and unused tickets
+GROUP BY
+	p.Id,
+	p.Name,
+	YEAR(t.DateCreated),
+	(ph.Price / ph.NumberOfTickets)
+ORDER BY
+	YEAR(t.DateCreated)
