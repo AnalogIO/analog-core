1000809ec3e477795be8a1b880b1ebde84ee76fb
Jonas Anker Rasmussen <jonasanker@gmail.com>
2022-01-11 23:15:47 +0100

    Minor documentation changes

--- coffeecard/CoffeeCard.Models/DataTransferObjects/User/TokenDTO.cs
+++ coffeecard/CoffeeCard.Models/DataTransferObjects/User/TokenDTO.cs
@@ -1,7 +1,20 @@
 namespace CoffeeCard.Models.DataTransferObjects.User
 {
+    /// <summary>
+    /// Login response with Bearer JWT Token
+    /// </summary>
+    /// <example>
+    /// {
+    ///     "token": "[no example provided]"
+    /// }
+    /// </example>
     public class TokenDto
     {
+        /// <summary>
+        /// Bearer JWT token used for authentication
+        /// </summary>
+        /// <value>Bearer Token</value>
+        /// <example>[no example provided]</example>
         public string Token { get; set; }
     }
 }
