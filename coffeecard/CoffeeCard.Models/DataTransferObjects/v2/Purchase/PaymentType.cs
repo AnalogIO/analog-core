f22ee3c6f4569518d740d89b595794b9c90b0116
Jonas Anker Rasmussen <jonasanker@gmail.com>
2022-01-12 15:45:39 +0100

    Initial API design and models for Purchases and MobilePay

---
+++ coffeecard/CoffeeCard.Models/DataTransferObjects/v2/Purchase/PaymentType.cs
@@ -0,0 +1,13 @@
+namespace CoffeeCard.Models.DataTransferObjects.v2.Purchase
+{
+    /// <summary>
+    /// PaymentType represents the type of Payment which is used to fulfill a purchase
+    /// </summary>
+    public enum PaymentType
+    {
+        /// <summary>
+        /// MobilePay App Payments
+        /// </summary>
+        MobilePay
+    }
+}
