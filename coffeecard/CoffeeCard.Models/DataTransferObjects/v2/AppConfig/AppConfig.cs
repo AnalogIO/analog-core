ec6f33c6ab1481642fe60a8de3c296f25376ffa4
Jonas Anker Rasmussen <jonasanker@gmail.com>
2022-02-01 21:48:20 +0100

    AppConfig Controller v2
    - GET operation returns App Configuration
    - Allow Anonymous

---
+++ coffeecard/CoffeeCard.Models/DataTransferObjects/v2/AppConfig/AppConfig.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using CoffeeCard.Common.Configuration;
+
+namespace CoffeeCard.Models.DataTransferObjects.v2.AppConfig
+{
+    /// <summary>
+    /// App Configuration
+    /// </summary>
+    /// <example>
+    /// {
+    ///     "environmentType": "Production"
+    /// }
+    /// </example>
+    public class AppConfig
+    {
+        /// <summary>
+        /// Environment type for indicating production or test system
+        /// </summary>
+        /// <value>Environment Type</value>
+        /// <example>Production</example>
+        [Required]
+        public EnvironmentType EnvironmentType { get; set; }
+    }
+}
