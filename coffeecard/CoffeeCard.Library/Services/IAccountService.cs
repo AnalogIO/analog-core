6f3a00fa6d10f5db100a6d569f61da88f1a03183
fremartini <frederik.martini@live.dk>
2022-03-05 14:52:30 +0100

    add delete endpoint to accountcontroller, add support for anonymizing users through email and add razor pages

--- coffeecard/CoffeeCard.Library/Services/IAccountService.cs
+++ coffeecard/CoffeeCard.Library/Services/IAccountService.cs
@@ -16,5 +16,8 @@ namespace CoffeeCard.Library.Services
         void UpdateExperience(int userId, int exp);
         Task ForgotPasswordAsync(string email);
         Task<bool> RecoverUserAsync(string token, string newPassword);
+        bool UserExists(string email);
+        void AnonymizeAccount(string email);
+        bool VerifyAnonymization(string token);
     }
 }
