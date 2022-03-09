1a53c62bffbab3a2afdfedcb5978a007a59ebe96
fremartini <frederik.martini@live.dk>
2022-03-09 17:06:12 +0100

    add error handling to verify email and verify delete

--- coffeecard/CoffeeCard.WebApi/Pages/VerifyDelete.cshtml.cs
+++ coffeecard/CoffeeCard.WebApi/Pages/VerifyDelete.cshtml.cs
@@ -1,3 +1,4 @@
+using System;
 using CoffeeCard.Library.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -17,17 +18,20 @@ namespace CoffeeCard.WebApi.Pages
 
         public IActionResult OnGet()
         {
-            var emailVerified = _accountService.AnonymizeAccount(Token);
-            if (emailVerified)
+            try
             {
+                _accountService.AnonymizeAccount(Token);
+                
                 TempData["resultHeader"] = "Success";
                 TempData["result"] = @"Your account has been successfully deleted";
-                return RedirectToPage("result");
             }
-
-            TempData["resultHeader"] = "Error";
-            TempData["result"] =
-                @"Looks like the link you used has expired or already been used.";
+            catch (Exception)
+            {
+                //FIXME: there was an error updating the user, how do we handle this?
+                TempData["resultHeader"] = "Error";
+                TempData["result"] =
+                    @"Looks like the link you used has expired or already been used.";                
+            }
             return RedirectToPage("result");
         }
     }
