edf9cec2073804c5e2cdb0a9ed6846fdf82d5fa0
Jonas Anker Rasmussen <jonasanker@gmail.com>
2022-03-13 13:24:14 +0100

    New GetTickets endpoint include ProductId

--- coffeecard/CoffeeCard.Library/Services/v2/ITicketService.cs
+++ coffeecard/CoffeeCard.Library/Services/v2/ITicketService.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using CoffeeCard.Models.DataTransferObjects.v2.Ticket;
 using CoffeeCard.Models.Entities;
 
 namespace CoffeeCard.Library.Services.v2
@@ -6,5 +8,7 @@ namespace CoffeeCard.Library.Services.v2
     public interface ITicketService
     {
         Task IssueTickets(Purchase purchase);
+
+        Task<List<TicketResponse>> GetTickets(User user, bool includeUsed);
     }
 }
---
+++ coffeecard/CoffeeCard.Models/DataTransferObjects/v2/Ticket/TicketResponse.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CoffeeCard.Models.DataTransferObjects.v2.Ticket
+{
+    /// <summary>
+    /// Representing a ticket for a product
+    /// </summary>
+    /// <example>
+    /// {
+    ///     "id": 122,
+    ///     "dateCreated": "2022-01-09T21:03:52.2283208Z",
+    ///     "dateUsed": "2022-01-09T21:03:52.2283208Z",
+    ///     "productId": 1,
+    ///     "productName: "Coffee"
+    /// }
+    /// </example>
+    public class TicketResponse
+    {
+        /// <summary>
+        /// Ticket Id
+        /// </summary>
+        /// <value>Ticket Id</value>
+        /// <example>122</example>
+        [Required]
+        public int Id { get; set; }
+        
+        /// <summary>
+        /// Issuing date time for ticket in Utc format
+        /// </summary>
+        /// <value>Issued date time</value>
+        /// <example>2022-01-09T21:03:52.2283208Z</example>
+        [Required]
+        public DateTime DateCreated { get; set; }
+        
+        /// <summary>
+        /// Used date time for ticket in Utc format
+        /// </summary>
+        /// <value>Used date time</value>
+        /// <example>2022-01-09T21:03:52.2283208Z</example>
+        [Required]
+        public DateTime? DateUsed { get; set; }
+        
+        /// <summary>
+        /// The Id of product a ticket is for
+        /// </summary>
+        /// <value>Product Id</value>
+        /// <example>1</example>
+        [Required]
+        public int ProductId { get; set; }
+        
+        /// <summary>
+        /// Name of product a ticket is for
+        /// </summary>
+        /// <value>Product name</value>
+        /// <example>Coffee</example>
+        [Required]
+        public string ProductName { get; set; }
+    }
+}
