b2c3af1b380fc59cb761200ff6459845bf8bf463
Frederik Martini <39860858+fremartini@users.noreply.github.com>
2022-04-06 07:16:16 +0200

    add endpoint to check if email is already registered (#78)
    * add endpoint to check if email is already registered

---
+++ coffeecard/CoffeeCard.Models/DataTransferObjects/v2/User/EmailExistsRequest.cs
@@ -0,0 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace CoffeeCard.Models.DataTransferObjects.v2.User
+{
+    /// <summary>
+    /// An email that should be checked if it already exists
+    /// </summary>
+    /// <example>
+    /// {
+    ///     "email": "johndoe@mail.com"
+    /// }
+    /// </example>
+    public class EmailExistsRequest
+    {
+        /// <summary>
+        /// Email
+        /// </summary>
+        /// <value>Email</value>
+        /// <example>johndoe@mail.com</example>
+        [EmailAddress]
+        [Required]
+        public string Email { get; set; }
+    }
+}
