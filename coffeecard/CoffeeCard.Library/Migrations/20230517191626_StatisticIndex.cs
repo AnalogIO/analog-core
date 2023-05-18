750f9a66a5454066710652adf3333aba11cfd01b
Jonas Anker Rasmussen <jonasanker@gmail.com>
2023-05-18 09:42:51 +0200

    Minor tweaks to Pull Request
    - Added index targeting to GetSortedStatistics query
    - Improve documentation on the fly

---
+++ coffeecard/CoffeeCard.Library/Migrations/20230517191626_StatisticIndex.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CoffeeCard.Library.Migrations
+{
+    public partial class StatisticIndex : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Statistics_Preset_ExpiryDate_SwipeCount_LastSwipe",
+                schema: "dbo",
+                table: "Statistics",
+                columns: new[] { "Preset", "ExpiryDate", "SwipeCount", "LastSwipe" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Statistics_Preset_ExpiryDate_SwipeCount_LastSwipe",
+                schema: "dbo",
+                table: "Statistics");
+        }
+    }
+}
--- coffeecard/CoffeeCard.Models/Entities/Statistic.cs
+++ coffeecard/CoffeeCard.Models/Entities/Statistic.cs
@@ -1,31 +1,73 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
 
 namespace CoffeeCard.Models.Entities
 {
-    /**
-    * This class will be used to optimize the queries to get statistics.
-    * The preset defines the kind of statistic entity and each preset has a corresponding `SwipeCount`, `SwipeRank` and `ExpiryDate`.
-    * This means that each user should have exactly 3 entries in the database after 1 or more swipes.
-    * Each preset should be updated accordingly to the current date and the ExpiryDate. If the ExpiryDate of a entry is exceeded, then the `SwipeCount`, `SwipeRank` and `ExpiryDate` should be reset.
-    **/
-    public enum StatisticPreset { Monthly, Semester, Total }
+    /// <summary>
+    /// This class will be used to optimize the queries to get statistics.
+    /// The preset defines the kind of statistic entity and each preset has a corresponding `SwipeCount`, `SwipeRank` and `ExpiryDate`.
+    /// This means that each user should have exactly 3 entries in the database after 1 or more swipes.
+    ///
+    /// Each preset should be updated accordingly to the current date and the ExpiryDate.
+    /// If the ExpiryDate of a entry is exceeded, then the `SwipeCount`, `SwipeRank` and `ExpiryDate` should be reset.
+    /// </summary>
+    public enum StatisticPreset
+    {
+        /// <summary>
+        /// Statistic preset for a month
+        /// </summary>
+        Monthly, 
+        /// <summary>
+        /// Statistic preset for a semester
+        /// </summary>
+        Semester, 
+        /// <summary>
+        /// All Statistics
+        /// </summary>
+        Total
+    }
 
+    /// <summary>
+    /// Statistic represent a user's swipe of tickets. A Statistic entry is valid within a given preset period unless it is the total statistics.
+    /// </summary>
+    [Index(nameof(Preset), nameof(ExpiryDate), nameof(SwipeCount), nameof(LastSwipe))]
     public class Statistic
     {
+        /// <summary>
+        /// Statistic Id
+        /// </summary>
         public int Id { get; set; }
         
+        /// <summary>
+        /// Preset. A uesr is expected to have one entry of each Preset type
+        /// </summary>
         public StatisticPreset Preset { get; set; }
         
+        /// <summary>
+        /// Number of swipes for preset and within expiry date
+        /// </summary>
         public int SwipeCount { get; set; }
         
+        /// <summary>
+        /// Last time a user swiped for a given preset
+        /// </summary>
         public DateTime LastSwipe { get; set; }
         
+        /// <summary>
+        /// Expiry date of statistic entry. When the expiry date has been exceeded, the statistic entry is reest
+        /// </summary>
         public DateTime ExpiryDate { get; set; }
 
+        /// <summary>
+        /// User Id which Statistic entry is for
+        /// </summary>
         [Column(name: "User_Id")]
         public int UserId { get; set; }
 
+        /// <summary>
+        /// Object reference to User
+        /// </summary>
         public virtual User User { get; set; }
     }
 }
