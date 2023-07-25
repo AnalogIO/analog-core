844217358969bdb54dacf2e6b94cafe028da79d7
Thomas Andersen <d6di6skcprs5@opayq.com>
2023-07-25 19:19:22 +0200

    Added some basic integration tests for demonstrating the new setup

---
+++ coffeecard/CoffeeCard.Tests.Integration/Controllers/Account/GetAccountTest.cs
@@ -0,0 +1,36 @@
+using System.Threading.Tasks;
+using CoffeeCard.Tests.Integration.WebApplication;
+using CoffeeCard.WebApi;
+using Xunit;
+using CoffeeCard.Models.DataTransferObjects.v2.User;
+using CoffeeCard.Tests.Common.Builders;
+using CoffeeCard.Models.Entities;
+
+namespace CoffeeCard.Tests.Integration.Controllers.Account {
+
+    public class GetAccountTest : BaseIntegrationTest
+    {
+		private const string GetAccountUrl  = "api/v2/account";
+        public GetAccountTest(CustomWebApplicationFactory<Startup> factory) : base(factory)
+        {
+        }
+
+		[Fact]
+		public async Task Get_account_succeeds_when_authenticated_for_existing_account()
+		{
+			var user = new UserBuilder().DefaultCustomer().Build();
+			await Context.Users.AddAsync(user);
+			await Context.SaveChangesAsync();
+			SetDefaultAuthHeader(user);
+
+			var response = await Client.GetAsync(GetAccountUrl);
+			var account = await DeserializeResponseAsync<UserResponse>(response);
+
+			Assert.Equal(user.Email, account.Email);
+			Assert.Equal(user.Name, account.Name);
+			Assert.Equal(user.Programme.FullName, account.Programme.FullName);
+			Assert.Equal(user.UserGroup.toUserRole(), account.Role);
+		}
+		
+    }
+}
