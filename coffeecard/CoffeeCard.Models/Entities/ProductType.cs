6a828ae919da845017638a08f27292edc61a9d83
Jonas Anker Rasmussen <jonasanker@gmail.com>
2023-09-14 20:57:43 +0200

    Code comment

--- coffeecard/CoffeeCard.Models/Entities/ProductType.cs
+++ coffeecard/CoffeeCard.Models/Entities/ProductType.cs
@@ -1,5 +1,8 @@
 namespace CoffeeCard.Models.Entities
 {
+    /// <summary>
+    /// Product type indicates a categorization/type of a given product
+    /// </summary>
     public enum ProductType
     {
         /// SingleUse
