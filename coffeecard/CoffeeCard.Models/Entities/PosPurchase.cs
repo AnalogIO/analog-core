ce86aecccc7778046cd298ada42d233250e11541
Jonas Anker Rasmussen <jonasanker@gmail.com>
2023-09-22 20:41:46 +0200

    Change typo, update format

--- coffeecard/CoffeeCard.Models/Entities/PosPurchase.cs
+++ coffeecard/CoffeeCard.Models/Entities/PosPurchase.cs
@@ -3,14 +3,15 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CoffeeCard.Models.Entities
 {
-    /// Represents a purchase issued by a barista at the point of sale in the cafe
-	public class PosPurhase{
-		/// The foreign key linking to a purchase
-		[Key, ForeignKey(nameof(Purchase))]
-		public int PurchaseId { get; set; }
+    /// Represents a purchase issued by a barista with the point of sale system in the cafe
+	public class PosPurhase
+    {
+        /// The foreign key linking to a purchase
+        [Key, ForeignKey(nameof(Purchase))]
+        public int PurchaseId { get; set; }
         /// The purchase that was issued by the barista
 		public virtual Purchase Purchase { get; set; }
-        /// The ITU intials of the issueing barista
-		public string BastiaInitials {get; set; }
-	}
+        /// The ITU intials of the barista issueing the product
+		public string BaristaInitials { get; set; }
+    }
 }
