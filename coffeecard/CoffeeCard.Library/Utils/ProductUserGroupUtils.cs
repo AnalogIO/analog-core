960209927e306bdc01839c59d34bdbf9f6942235
Frederik Petersen <fred.p@live.dk>
2023-10-12 19:03:40 +0200

    Fix sonarcloud bug :bug:

--- coffeecard/CoffeeCard.Library/Utils/ProductUserGroupUtils.cs
+++ coffeecard/CoffeeCard.Library/Utils/ProductUserGroupUtils.cs
@@ -1,17 +1,20 @@
 ﻿using System.Linq;
 using CoffeeCard.Models.Entities;
 
-public static class ProductUserGroupUtils
+namespace CoffeeCard.Library.Utils
 {
-    /// <summary>
-    /// Determines whether a product is a perk or not.
-    /// A product is a perk if it is not available for regular customers.
-    /// </summary>
-    /// <param name="context">The DbContext to use for querying the database.</param>
-    /// <param name="product">The product to check.</param>
-    /// <returns>True if the product is a perk, false otherwise.</returns>
-    public static bool isPerk(this Product product)
+    public static class ProductUserGroupUtils
     {
-        return product.ProductUserGroup.Any(pug => pug.UserGroup != UserGroup.Customer);
+        /// <summary>
+        /// Determines whether a product is a perk or not.
+        /// A product is a perk if it is not available for regular customers.
+        /// </summary>
+        /// <param name="context">The DbContext to use for querying the database.</param>
+        /// <param name="product">The product to check.</param>
+        /// <returns>True if the product is a perk, false otherwise.</returns>
+        public static bool isPerk(this Product product)
+        {
+            return product.ProductUserGroup.Any(pug => pug.UserGroup != UserGroup.Customer);
+        }
     }
 }
