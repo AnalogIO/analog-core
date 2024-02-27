3ebc40cecba925d63ac7a3aa2f78ea528840498f
A-Guldborg <aguh@itu.dk>
2024-02-27 19:21:46 +0100

    Adds index on User.UserGroup

--- coffeecard/CoffeeCard.Library/Migrations/20240227174524_AddUserGroupIndexToUsers.Designer.cs
+++ coffeecard/CoffeeCard.Library/Migrations/20240227174524_AddUserGroupIndexToUsers.Designer.cs
@@ -4,6 +4,7 @@ using CoffeeCard.Library.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CoffeeCard.Library.Migrations
 {
     [DbContext(typeof(CoffeeCardContext))]
-    partial class CoffeeCardContextModelSnapshot : ModelSnapshot
+    [Migration("20240227174524_AddUserGroupIndexToUsers")]
+    partial class AddUserGroupIndexToUsers
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
