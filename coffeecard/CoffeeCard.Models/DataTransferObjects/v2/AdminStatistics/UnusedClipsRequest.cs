7474917a758d11b23a29f8f103f16a7f1435ba72
Jonas Anker Rasmussen <jonasanker@gmail.com>
2024-03-12 19:23:31 +0100

    Fix formatting

--- coffeecard/CoffeeCard.Models/DataTransferObjects/v2/AdminStatistics/UnusedClipsRequest.cs
+++ coffeecard/CoffeeCard.Models/DataTransferObjects/v2/AdminStatistics/UnusedClipsRequest.cs
@@ -10,16 +10,16 @@ namespace CoffeeCard.Models.DataTransferObjects.v2.AdminStatistics
         /// <summary>
         /// The start date of unused tickets query.
         /// </summary>
-        /// <value>Start Date.</value>
+        /// <value>Start Date</value>
         /// <example>2021-02-08</example>
         public required DateTime StartDate { get; set; }
 
-        /// <summary>
-        /// The end date of unused tickets query.
-        /// </summary>
-        /// <value>End Date.</value>
-        /// <example>2024-02-08</example>
-        public required DateTime EndDate { get; set; }
+    /// <summary>
+    /// The end date of unused tickets query.
+    /// </summary>
+    /// <value>End Date</value>
+    /// <example>2024-02-08</example>
+    public required DateTime EndDate { get; set; }
     }
 }
 
