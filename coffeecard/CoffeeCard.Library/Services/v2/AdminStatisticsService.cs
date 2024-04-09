682663be93d7836d2361f01a58c475c413d8d973
Hubert Wójcik <81220416+HubertWojcik10@users.noreply.github.com>
2024-04-09 18:58:10 +0200

    add an unused clips endpoint to a new controller - user statistics (#257)
    Co-authored-by: Jonas Anker Rasmussen <jonasanker@gmail.com>

---
+++ coffeecard/CoffeeCard.Library/Services/v2/AdminStatisticsService.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoffeeCard.Library.Persistence;
+using CoffeeCard.Models.DataTransferObjects.v2.AdminStatistics;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoffeeCard.Library.Services.v2
+{
+    public class AdminStatisticsService : IAdminStatisticsService
+    {
+        private readonly CoffeeCardContext _context;
+
+        public AdminStatisticsService(CoffeeCardContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<UnusedClipsResponse>> GetUnusedClips(UnusedClipsRequest unusedClipsRequest)
+        {
+            var startDate = unusedClipsRequest.StartDate;
+            var endDate = unusedClipsRequest.EndDate;
+
+            var tickets = await _context.Tickets
+                .Where(t => t.DateCreated >= startDate && t.DateCreated <= endDate && t.IsUsed == false)
+                .GroupBy(ticket => ticket.ProductId)
+                .Select(group => new UnusedClipsResponse
+                {
+                    ProductId = group.Key,
+                    ProductName = group.First().Purchase.ProductName,
+                    TicketsLeft = group.Count(),
+                    UnusedPurchasesValue = group.Sum(item => (1 / (decimal)item.Purchase.NumberOfTickets) * item.Purchase.Price),
+                })
+                .ToListAsync();
+
+            return tickets;
+        }
+    }
+}
---
+++ coffeecard/CoffeeCard.Library/Services/v2/IAdminStatisticsService.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using CoffeeCard.Models.DataTransferObjects.v2.AdminStatistics;
+
+namespace CoffeeCard.Library.Services.v2
+{
+    public interface IAdminStatisticsService
+    {
+        public Task<List<UnusedClipsResponse>> GetUnusedClips(UnusedClipsRequest unusedClipsRequest);
+    }
+}
