53bee39285f4c47c5c00b7049d57d71f5881fe42
Andreas Trøstrup <andreastrstrup@gmail.com>
2024-10-22 20:42:54 +0200

    Run dotnet format

--- coffeecard/CoffeeCard.Library/Migrations/20241022182630_AddStatusToTickets.cs
+++ coffeecard/CoffeeCard.Library/Migrations/20241022182630_AddStatusToTickets.cs
@@ -17,17 +17,17 @@ namespace CoffeeCard.Library.Migrations
                 type: "int",
                 nullable: false,
                 defaultValue: 0);
-            
+
             // Update the Status column according to the IsUsed column
             migrationBuilder.Sql("UPDATE dbo.Tickets SET Status = 1 WHERE IsUsed = 1");
         }
-        
+
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
             // Update the IsUsed column according to the Status column
             migrationBuilder.Sql("UPDATE dbo.Tickets SET IsUsed = 1 WHERE Status = 1 OR Status = 2");
-            
+
             migrationBuilder.DropColumn(
                 name: "Status",
                 schema: "dbo",
