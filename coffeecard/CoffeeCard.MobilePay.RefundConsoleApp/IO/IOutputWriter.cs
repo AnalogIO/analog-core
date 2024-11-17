a38dbdc30175d19040f8b4b7abbbd2cc30d3e8bb
Omid Marfavi <21163286+marfavi@users.noreply.github.com>
2024-11-17 12:48:37 +0100

    Remove MobilePay RefundConsoleApp project

--- coffeecard/CoffeeCard.MobilePay.RefundConsoleApp/IO/IOutputWriter.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace CoffeeCard.MobilePay.RefundConsoleApp.IO
-{
-    public interface IOutputWriter<in T>
-    {
-        Task WriteToFileAsync(T refunds);
-    }
-}
