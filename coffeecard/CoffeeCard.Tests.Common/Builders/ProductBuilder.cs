e5ba8b9da88e5f4cafc35f5d161f55c64a20b780
Thomas Andersen <d6di6skcprs5@opayq.com>
2024-11-19 18:13:10 +0100

    Refactor/unit tests to builders (#303)
    Add solution folders, and refactor integration test
    Updated loginLimiterTest to use builders
    Updated accountServiceTest to use builders and shared base
    Re-wrote productService tests to use builders, and added more builders

--- coffeecard/CoffeeCard.Tests.Common/Builders/ProductBuilder.cs
+++ coffeecard/CoffeeCard.Tests.Common/Builders/ProductBuilder.cs
@@ -8,15 +8,23 @@ namespace CoffeeCard.Tests.Common.Builders
         public static ProductBuilder Simple()
         {
             return new ProductBuilder()
+                .WithPrice(f => f.Random.Int(0, 200))
+                .WithVisible(true)
+                .WithExperienceWorth(f => f.Random.Int(0, 200))
                 .WithName(f => f.Commerce.ProductName())
                 .WithDescription(f => f.Commerce.ProductDescription())
                 .WithNumberOfTickets(f => f.PickRandom(1, 10))
-                .WithProductUserGroup(new List<ProductUserGroup>());
+                .WithEligibleMenuItems([])
+                .WithMenuItemProducts([])
+                .WithProductUserGroup([]);
         }
 
         public static ProductBuilder Typical()
         {
-            return Simple();
+            return Simple()
+                .WithMenuItemProducts(MenuItemProductBuilder.Simple().Build(1))
+                .WithEligibleMenuItems(MenuItemBuilder.Simple().Build(1))
+                .WithProductUserGroup(ProductUserGroupBuilder.Simple().Build(1));
         }
     }
 }
