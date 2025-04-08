f04bf55be342af64ac2b8749da9427398d312eed
Andreas Trøstrup <andreastrstrup@gmail.com>
2025-04-08 15:36:29 +0200

    doc

--- coffeecard/CoffeeCard.Models/DataTransferObjects/MobilePay/GetWebhookResponse.cs
+++ coffeecard/CoffeeCard.Models/DataTransferObjects/MobilePay/GetWebhookResponse.cs
@@ -11,12 +11,18 @@ public class GetWebhookResponse
     /// <summary>
     /// The URL to which MobilePay will send webhook notifications.
     /// </summary>
+    /// <example>
+    /// https://core.dev.analogio.dk/api/v2/mobilepay
+    /// </example>
     [Required]
     public required Uri Url { get; set; }
 
     /// <summary>
     /// The unique identifier of the registered webhook.
     /// </summary>
+    /// <example>
+    /// 3b69088a-0d86-4c24-9187-351e478513fd
+    /// </example>
     [Required]
     public required Guid WebhookId { get; set; }
 }
