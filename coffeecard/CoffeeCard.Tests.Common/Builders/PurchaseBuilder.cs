c57528079150d8e38e89fe9ead3d4a1c5374194b
Thomas Andersen <d6di6skcprs5@opayq.com>
2026-03-25 19:19:37 +0100

    Fix validation errors on builders

--- coffeecard/CoffeeCard.Tests.Common/Builders/PurchaseBuilder.cs
+++ coffeecard/CoffeeCard.Tests.Common/Builders/PurchaseBuilder.cs
@@ -1,3 +1,4 @@
+using CoffeeCard.Models.DataTransferObjects.v2.Purchase;
 using CoffeeCard.Models.Entities;
 
 namespace CoffeeCard.Tests.Common.Builders
@@ -12,8 +13,14 @@ namespace CoffeeCard.Tests.Common.Builders
                 .WithPurchasedBy(purchasedBy)
                 .WithProductName(f => f.Commerce.ProductName())
                 .WithTickets(new List<Ticket>())
-                .WithTickets(new List<Ticket>())
-                .WithNumberOfTickets(0);
+                .WithNumberOfTickets(0)
+                .WithVoucher(_ => null)
+                .WithProduct(ProductBuilder.Simple().Build())
+                .WithPrice(f => f.Random.Int(1, 200))
+                .WithDateCreated(f => f.Date.Past())
+                .WithStatus(f => f.Random.Enum<PurchaseStatus>())
+                .WithType(f => f.Random.Enum<PurchaseType>())
+                .WithOrderId(f => f.Random.Guid().ToString());
         }
 
         public static PurchaseBuilder Typical()
--- coffeecard/CoffeeCard.Tests.Common/Builders/VoucherBuilder.cs
+++ coffeecard/CoffeeCard.Tests.Common/Builders/VoucherBuilder.cs
@@ -8,12 +8,19 @@ namespace CoffeeCard.Tests.Common.Builders
         public static VoucherBuilder Simple()
         {
             var product = ProductBuilder.Simple().Build();
-            return new VoucherBuilder().WithProduct(product).WithUser(f => null);
+            return new VoucherBuilder()
+                .WithProduct(product)
+                .WithPurchase(_ => null)
+                .WithUser(_ => null)
+                .WithDescription(_ => null)
+                .WithRequester(_ => null);
         }
 
         public static VoucherBuilder Typical()
         {
-            return Simple();
+            return Simple()
+                .WithPurchase(PurchaseBuilder.Simple().WithType(PurchaseType.Voucher).Build())
+                .WithUser(UserBuilder.DefaultCustomer().Build());
         }
     }
 }
