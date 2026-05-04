58b15aae00fe4c4b5a6b3468b01162b828204103
Omid Marfavi <21163286+marfavi@users.noreply.github.com>
2026-05-04 16:36:29 +0200

    Use Builders for test data

--- coffeecard/CoffeeCard.Tests.Unit/Services/v2/StatisticsServiceTests.cs
+++ coffeecard/CoffeeCard.Tests.Unit/Services/v2/StatisticsServiceTests.cs
@@ -8,6 +8,7 @@ using CoffeeCard.Library.Services.v2;
 using CoffeeCard.Library.Utils;
 using CoffeeCard.Models.DataTransferObjects.v2.Purchase;
 using CoffeeCard.Models.Entities;
+using CoffeeCard.Tests.Common.Builders;
 using Microsoft.EntityFrameworkCore;
 using Moq;
 using Xunit;
@@ -36,199 +37,162 @@ namespace CoffeeCard.Tests.Unit.Services.v2
                 environmentSettings
             );
 
-            var swu = new Programme
-            {
-                Id = 1,
-                ShortName = "SWU",
-                FullName = "Software Development",
-                SortPriority = 1,
-            };
-            var ds = new Programme
-            {
-                Id = 2,
-                ShortName = "DS",
-                FullName = "Data Science",
-                SortPriority = 2,
-            };
+            var user = UserBuilder.DefaultCustomer().WithId(1).Build();
 
-            var user = new User
-            {
-                Id = 1,
-                Name = "User1",
-                Email = "user1@itu.dk",
-                Password = "password",
-                Salt = "salt",
-                DateCreated = new DateTime(2025, 1, 1),
-                IsVerified = true,
-                PrivacyActivated = false,
-                UserGroup = UserGroup.Customer,
-                UserState = UserState.Active,
-                ProgrammeId = swu.Id,
-                Programme = swu,
-            };
+            var otherUser = UserBuilder.DefaultCustomer().WithId(2).Build();
 
-            var otherUser = new User
-            {
-                Id = 2,
-                Name = "User2",
-                Email = "user2@itu.dk",
-                Password = "password",
-                Salt = "salt",
-                DateCreated = new DateTime(2025, 1, 1),
-                IsVerified = true,
-                PrivacyActivated = false,
-                UserGroup = UserGroup.Customer,
-                UserState = UserState.Active,
-                ProgrammeId = ds.Id,
-                Programme = ds,
-            };
-
-            context.Programmes.AddRange(swu, ds);
             context.Users.AddRange(user, otherUser);
 
             var currentTime = new DateTime(2026, 5, 3, 12, 0, 0, DateTimeKind.Utc);
 
-            var latte = new MenuItem { Id = 1, Name = "Latte" };
-            var americano = new MenuItem { Id = 2, Name = "Americano" };
-            var espresso = new MenuItem { Id = 3, Name = "Espresso" };
+            var latte = MenuItemBuilder.Simple().WithId(1).WithName("Latte").Build();
+            var americano = MenuItemBuilder.Simple().WithId(2).WithName("Americano").Build();
+            var espresso = MenuItemBuilder.Simple().WithId(3).WithName("Espresso").Build();
 
             context.MenuItems.AddRange(latte, americano, espresso);
 
-            var purchases = new List<Purchase>
-            {
-                new Purchase
-                {
-                    Id = 1,
-                    ProductName = "Large",
-                    ProductId = 1,
-                    Price = 20,
-                    NumberOfTickets = 1,
-                    OrderId = "order-1",
-                    Status = PurchaseStatus.Completed,
-                    Type = PurchaseType.Free,
-                    PurchasedById = user.Id,
-                    PurchasedBy = user,
-                    DateCreated = currentTime.AddDays(-1),
-                },
-                new Purchase
-                {
-                    Id = 2,
-                    ProductName = "Small",
-                    ProductId = 2,
-                    Price = 20,
-                    NumberOfTickets = 1,
-                    OrderId = "order-2",
-                    Status = PurchaseStatus.Completed,
-                    Type = PurchaseType.Free,
-                    PurchasedById = user.Id,
-                    PurchasedBy = user,
-                    DateCreated = currentTime.AddDays(-2),
-                },
-                new Purchase
-                {
-                    Id = 3,
-                    ProductName = "Large",
-                    ProductId = 1,
-                    Price = 20,
-                    NumberOfTickets = 1,
-                    OrderId = "order-3",
-                    Status = PurchaseStatus.Completed,
-                    Type = PurchaseType.Free,
-                    PurchasedById = user.Id,
-                    PurchasedBy = user,
-                    DateCreated = currentTime.AddDays(-3),
-                },
-                new Purchase
-                {
-                    Id = 4,
-                    ProductName = "Small",
-                    ProductId = 3,
-                    Price = 20,
-                    NumberOfTickets = 1,
-                    OrderId = "order-4",
-                    Status = PurchaseStatus.Completed,
-                    Type = PurchaseType.Free,
-                    PurchasedById = otherUser.Id,
-                    PurchasedBy = otherUser,
-                    DateCreated = currentTime.AddDays(-1),
-                },
-            };
+            var largeProduct = ProductBuilder.Simple().WithId(1).WithName("Large").Build();
+            var smallProduct = ProductBuilder.Simple().WithId(2).WithName("Small").Build();
+            var fancyProduct = ProductBuilder.Simple().WithId(3).WithName("Fancy").Build();
+
+            context.Products.AddRange(largeProduct, smallProduct, fancyProduct);
+
+            var purchase1 = PurchaseBuilder
+                .Simple()
+                .WithId(1)
+                .WithProductName("Large")
+                .WithProductId(largeProduct.Id)
+                .WithProduct(largeProduct)
+                .WithPrice(20)
+                .WithNumberOfTickets(1)
+                .WithOrderId("order-1")
+                .WithStatus(PurchaseStatus.Completed)
+                .WithType(PurchaseType.Free)
+                .WithPurchasedById(user.Id)
+                .WithPurchasedBy(user)
+                .WithDateCreated(currentTime.AddDays(-1))
+                .Build();
+            var purchase2 = PurchaseBuilder
+                .Simple()
+                .WithId(2)
+                .WithProductName("Small")
+                .WithProductId(smallProduct.Id)
+                .WithProduct(smallProduct)
+                .WithPrice(20)
+                .WithNumberOfTickets(1)
+                .WithOrderId("order-2")
+                .WithStatus(PurchaseStatus.Completed)
+                .WithType(PurchaseType.Free)
+                .WithPurchasedById(user.Id)
+                .WithPurchasedBy(user)
+                .WithDateCreated(currentTime.AddDays(-2))
+                .Build();
+            var purchase3 = PurchaseBuilder
+                .Simple()
+                .WithId(3)
+                .WithProductName("Large")
+                .WithProductId(largeProduct.Id)
+                .WithProduct(largeProduct)
+                .WithPrice(20)
+                .WithNumberOfTickets(1)
+                .WithOrderId("order-3")
+                .WithStatus(PurchaseStatus.Completed)
+                .WithType(PurchaseType.Free)
+                .WithPurchasedById(user.Id)
+                .WithPurchasedBy(user)
+                .WithDateCreated(currentTime.AddDays(-3))
+                .Build();
+            var purchase4 = PurchaseBuilder
+                .Simple()
+                .WithId(4)
+                .WithProductName("Small")
+                .WithProductId(fancyProduct.Id)
+                .WithProduct(fancyProduct)
+                .WithPrice(20)
+                .WithNumberOfTickets(1)
+                .WithOrderId("order-4")
+                .WithStatus(PurchaseStatus.Completed)
+                .WithType(PurchaseType.Free)
+                .WithPurchasedById(otherUser.Id)
+                .WithPurchasedBy(otherUser)
+                .WithDateCreated(currentTime.AddDays(-1))
+                .Build();
 
             var tickets = new List<Ticket>
             {
-                new Ticket
-                {
-                    Id = 1,
-                    DateCreated = currentTime.AddDays(-3),
-                    DateUsed = currentTime.AddDays(-3),
-                    ProductId = 1,
-                    Status = TicketStatus.Used,
-                    OwnerId = user.Id,
-                    Owner = user,
-                    PurchaseId = purchases[0].Id,
-                    Purchase = purchases[0],
-                    UsedOnMenuItemId = latte.Id,
-                    UsedOnMenuItem = latte,
-                },
-                new Ticket
-                {
-                    Id = 2,
-                    DateCreated = currentTime.AddDays(-2),
-                    DateUsed = currentTime.AddDays(-2),
-                    ProductId = 2,
-                    Status = TicketStatus.Used,
-                    OwnerId = user.Id,
-                    Owner = user,
-                    PurchaseId = purchases[1].Id,
-                    Purchase = purchases[1],
-                    UsedOnMenuItemId = americano.Id,
-                    UsedOnMenuItem = americano,
-                },
-                new Ticket
-                {
-                    Id = 3,
-                    DateCreated = currentTime.AddDays(-1),
-                    DateUsed = currentTime.AddDays(-1),
-                    ProductId = 1,
-                    Status = TicketStatus.Used,
-                    OwnerId = user.Id,
-                    Owner = user,
-                    PurchaseId = purchases[2].Id,
-                    Purchase = purchases[2],
-                    UsedOnMenuItemId = latte.Id,
-                    UsedOnMenuItem = latte,
-                },
-                new Ticket
-                {
-                    Id = 4,
-                    DateCreated = currentTime,
-                    DateUsed = currentTime,
-                    ProductId = 3,
-                    Status = TicketStatus.Used,
-                    OwnerId = otherUser.Id,
-                    Owner = otherUser,
-                    PurchaseId = purchases[3].Id,
-                    Purchase = purchases[3],
-                    UsedOnMenuItemId = espresso.Id,
-                    UsedOnMenuItem = espresso,
-                },
-                new Ticket
-                {
-                    Id = 5,
-                    DateCreated = currentTime,
-                    DateUsed = currentTime,
-                    ProductId = 3,
-                    Status = TicketStatus.Used,
-                    OwnerId = otherUser.Id,
-                    Owner = otherUser,
-                    PurchaseId = purchases[3].Id,
-                    Purchase = purchases[3],
-                    UsedOnMenuItemId = espresso.Id,
-                    UsedOnMenuItem = espresso,
-                },
+                TicketBuilder
+                    .Simple()
+                    .WithId(1)
+                    .WithDateCreated(currentTime.AddDays(-3))
+                    .WithDateUsed(currentTime.AddDays(-3))
+                    .WithProductId(largeProduct.Id)
+                    .WithStatus(TicketStatus.Used)
+                    .WithOwnerId(user.Id)
+                    .WithOwner(user)
+                    .WithPurchaseId(purchase1.Id)
+                    .WithPurchase(purchase1)
+                    .WithUsedOnMenuItemId(latte.Id)
+                    .WithUsedOnMenuItem(latte)
+                    .Build(),
+                TicketBuilder
+                    .Simple()
+                    .WithId(2)
+                    .WithDateCreated(currentTime.AddDays(-2))
+                    .WithDateUsed(currentTime.AddDays(-2))
+                    .WithProductId(smallProduct.Id)
+                    .WithStatus(TicketStatus.Used)
+                    .WithOwnerId(user.Id)
+                    .WithOwner(user)
+                    .WithPurchaseId(purchase2.Id)
+                    .WithPurchase(purchase2)
+                    .WithUsedOnMenuItemId(americano.Id)
+                    .WithUsedOnMenuItem(americano)
+                    .Build(),
+                TicketBuilder
+                    .Simple()
+                    .WithId(3)
+                    .WithDateCreated(currentTime.AddDays(-1))
+                    .WithDateUsed(currentTime.AddDays(-1))
+                    .WithProductId(largeProduct.Id)
+                    .WithStatus(TicketStatus.Used)
+                    .WithOwnerId(user.Id)
+                    .WithOwner(user)
+                    .WithPurchaseId(purchase3.Id)
+                    .WithPurchase(purchase3)
+                    .WithUsedOnMenuItemId(latte.Id)
+                    .WithUsedOnMenuItem(latte)
+                    .Build(),
+                TicketBuilder
+                    .Simple()
+                    .WithId(4)
+                    .WithDateCreated(currentTime)
+                    .WithDateUsed(currentTime)
+                    .WithProductId(fancyProduct.Id)
+                    .WithStatus(TicketStatus.Used)
+                    .WithOwnerId(otherUser.Id)
+                    .WithOwner(otherUser)
+                    .WithPurchaseId(purchase4.Id)
+                    .WithPurchase(purchase4)
+                    .WithUsedOnMenuItemId(espresso.Id)
+                    .WithUsedOnMenuItem(espresso)
+                    .Build(),
+                TicketBuilder
+                    .Simple()
+                    .WithId(5)
+                    .WithDateCreated(currentTime)
+                    .WithDateUsed(currentTime)
+                    .WithProductId(fancyProduct.Id)
+                    .WithStatus(TicketStatus.Used)
+                    .WithOwnerId(otherUser.Id)
+                    .WithOwner(otherUser)
+                    .WithPurchaseId(purchase4.Id)
+                    .WithPurchase(purchase4)
+                    .WithUsedOnMenuItemId(espresso.Id)
+                    .WithUsedOnMenuItem(espresso)
+                    .Build(),
             };
 
-            context.Purchases.AddRange(purchases);
+            context.Purchases.AddRange(purchase1, purchase2, purchase3, purchase4);
             context.Tickets.AddRange(tickets);
             await context.SaveChangesAsync();
 
