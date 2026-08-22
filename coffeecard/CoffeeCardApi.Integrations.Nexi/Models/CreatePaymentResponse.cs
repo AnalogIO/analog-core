f50eb3ec39583b72f8ecf9722b300d582af81fcf
Thomas Andersen <d6di6skcprs5@opayq.com>
2026-08-22 21:33:00 +0200

    Remove manual client

--- coffeecard/CoffeeCardApi.Integrations.Nexi/Models/CreatePaymentResponse.cs
+++
@@ -1,7 +0,0 @@
-namespace CoffeeCardApi.Integrations.Nexi.Models;
-
-public class CreatePaymentResponse
-{
-    public required string PaymentId { get; set; }
-    public required string HostedPaymentPageUrl { get; set; }
-}
