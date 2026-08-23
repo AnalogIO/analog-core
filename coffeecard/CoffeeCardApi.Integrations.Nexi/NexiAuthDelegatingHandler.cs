71da37d9be2ebfc4b6151c031fd4cb81392eda6d
Thomas Andersen <d6di6skcprs5@opayq.com>
2026-08-23 18:13:15 +0200

    Override both sends in the delegatingHandler

--- coffeecard/CoffeeCardApi.Integrations.Nexi/NexiAuthDelegatingHandler.cs
+++ coffeecard/CoffeeCardApi.Integrations.Nexi/NexiAuthDelegatingHandler.cs
@@ -11,13 +11,27 @@ public class NexiAuthDelegatingHandler : DelegatingHandler
         _settings = settings;
     }
 
+    protected override HttpResponseMessage Send(
+        HttpRequestMessage request,
+        CancellationToken cancellationToken
+    )
+    {
+        ApplyNexiAuthHeader(request);
+        return base.Send(request, cancellationToken);
+    }
+
     protected override Task<HttpResponseMessage> SendAsync(
         HttpRequestMessage request,
         CancellationToken cancellationToken
     )
     {
+        ApplyNexiAuthHeader(request);
+        return base.SendAsync(request, cancellationToken);
+    }
+
+    private void ApplyNexiAuthHeader(HttpRequestMessage request)
+    {
         // Nexi expects no scheme stated for the auth header
         request.Headers.Add("Authorization", _settings.ApiKey);
-        return base.SendAsync(request, cancellationToken);
     }
 }
